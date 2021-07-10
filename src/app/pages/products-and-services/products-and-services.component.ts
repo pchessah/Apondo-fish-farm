fbc455c147a512cb7627a98a15452c2d9bbace89
Paul Chesa <paulchesa1@gmail.com>
2021-07-10 08:34:05 +0300

    products and services page done

--- src/app/pages/products-and-services/products-and-services.component.ts
+++ src/app/pages/products-and-services/products-and-services.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products-and-services',
   templateUrl: './products-and-services.component.html',
-  styleUrls: ['./products-and-services.component.scss']
+  styleUrls: ['./products-and-services.component.scss'],
+  styles: [`
+  ::ng-deep .panel-header > .mat-expansion-indicator:after {
+    color: white;
+  }
+`],
 })
 export class ProductsAndServicesComponent implements OnInit {
   panelOpenState = false;
   constructor() { }
+
 
   ngOnInit(): void {
   }
